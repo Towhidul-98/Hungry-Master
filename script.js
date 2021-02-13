5ee6c3f7d870a73db1a85279c0407de929820cc5
Towhidul Islam <towhidulrafi1999@gmail.com>
2021-02-13 22:44:17 +0600

    second

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-function clickDiv(){
+function mealList(){
     fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     .then( Response => Response.json() )
     .then( data => displayItems(data))
 }
-
-clickDiv();
-
+mealList();
 function displayItems(meals){
     const itemsDiv= document.getElementById("displayZone");
     const categoryArray = meals.categories;
     categoryArray.forEach( mealItem => {
-        //console.log(23)
         const itemDiv = document.createElement('div');
         itemDiv.className= 'item';
-        itemDiv.innerText = mealItem.strCategory;
-        itemsDiv.appendChild(itemDiv)
+        
+        const elements = `
+            <img src="${mealItem.strCategoryThumb}" class="img-property">
+            <p>${mealItem.strCategory}</p>
+        `
+        
+        itemDiv.innerHTML = elements;
+        itemDiv.onclick= () =>displayMealDetail(mealItem);
+        itemsDiv.appendChild(itemDiv);
+
     });
-    // for (let i = 0; i < meals.categories.length; i++) {
-    //     const mealItem = meals.categories[i].strCategory;
-    //     const itemDiv = document.createElement('div');
-    //     itemDiv.className= 'item';
-    //     itemDiv.innerText = mealItem;
-    //     itemsDiv.appendChild(itemDiv)
-    // }
+}
+
+const displayMealDetail = name => {
+    console.log(name.strCategory)
 }
